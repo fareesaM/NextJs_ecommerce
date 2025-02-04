aa7055a6d654be68e5a100261154707ffcb5fa3f
fareesaM <fareesamas@gmail.com>
2025-02-04 16:34:12 +0300

    fixd

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,19 +1,15 @@
-
 "use client"; // ✅ Client Component
 
 import { useDispatch } from "react-redux";
-import { addToCart } from "@/lib/features/cartSlice";
-
+// import { addToCart } from "@/lib/features/cartSlice";
 import { Product } from "./types";
-
-
 
 export default function AddToCartButton({ product }: { product: Product }) {
   const dispatch = useDispatch();
 
   return (
     <button
-      
+     
       className="w-full bg-blue-500 text-white px-4 py-2 rounded mt-2"
     >
       Add to Cart
